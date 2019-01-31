726c0cd1b3ffc3e5a334d1e39af438c2d742dfe6
aghaynes <aghaynes@gmail.com>
2019-01-31 17:23:53 +0100

    checks for whether a folder is under SVN control

--- R/svn.R
+++ R/svn.R
@@ -1,44 +1,57 @@
-#* ******************************************** */ 
-#* Study: GENERIC CODE                        */ 
-#* Author: Alan Haynes                          */
-#* Purpose: Function to prepare report SVN statement   */  
-#* Date created: 30.01.2019                     */ 
-#* Last update:                       */ 
-#* External routines: None                      */ 
-#* ******************************************** */ 
 
+
+#' get details on the status of an svn repo
+#'
+#' @param path_to_repo path to the folder whose status to query (if NULL it uses the working directory)
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+#' svn_details()
+#'
+#'
 svn_details <- function(path_to_repo = NULL){
   if(is.null(path_to_repo)) path_to_repo <- ""
-  
+
+  control <- sapply(path_to_repo, .svn_control)
+  if(!any(control)) stop("'path_to_repo' not under SVN control")
+
   res <- sapply(path_to_repo, function(x) {
-    stat <- paste("svn status", x)
-    info <- paste("svn info", x)
-    stat <- system(stat, intern = TRUE)
-    info <- system(info, intern = TRUE)
-    if(x == "") x <- getwd()
-    
-    rel <- info[grepl("Relative URL", info)]
-    rel <- gsub("Relative URL: ", "", rel)
-    
-    # identify repo
-    ss <- unlist(strsplit(info[grepl("Repository Root", info)], "/"))
-    repo_name <- trimws(ss[length(ss)])
-    
-    # identify revision
-    ss <- unlist(strsplit(info[grepl("^Revision", info)], ":"))
-    rev_no <- trimws(ss[length(ss)])
-    
-    # changes?
-    status <- substring(stat, 1, 1)
-    changes <- any(c("A", "M", "D") %in% status)
-    changetxt <- ifelse(changes, "CAUTION: REPO HAS CHANGED SINCE LAST COMMIT", "")
-    txt <- sprintf("Folder %s is at revision %s from the %s SVN repository. %s", x, rev_no, repo_name, changetxt)
+    control <- .svn_control(x)
+    if(control){
+      stat <- paste("svn status", x)
+      info <- paste("svn info", x)
+      stat <- system(stat, intern = TRUE)
+      info <- system(info, intern = TRUE)
+      if(x == "") x <- getwd()
+
+      rel <- info[grepl("Relative URL", info)]
+      rel <- gsub("Relative URL: ", "", rel)
+
+      # identify repo
+      ss <- unlist(strsplit(info[grepl("Repository Root", info)], "/"))
+      repo_name <- trimws(ss[length(ss)])
+
+      # identify revision
+      ss <- unlist(strsplit(info[grepl("^Revision", info)], ":"))
+      rev_no <- trimws(ss[length(ss)])
+
+      # changes?
+      status <- substring(stat, 1, 1)
+      changes <- any(c("A", "M", "D") %in% status)
+      changetxt <- ifelse(changes, "CAUTION: REPO HAS CHANGED SINCE LAST COMMIT", "")
+      txt <- sprintf("Folder %s is at revision %s from the %s SVN repository. %s", x, rev_no, repo_name, changetxt)
+    } else {
+      if(x == "") x <- getwd()
+      txt <- sprintf("Folder %s is not under SVN control", x)
+    }
     txt
   })
-  
+
   paste(unlist(res), collapse = "\n")
-  
-  
+
+
 }
 
 
@@ -50,12 +63,28 @@ svn_details("ext/helper")
 svn_details(c("", "ext/r", "ext/ado"))
 
 path_to_repo <- c("", "ext/r", "ext/ado", "ext/helper")
-svn_details(path_to_repo)
+svn_details(c(path_to_repo, ""))
 
 path_to_repo <- "ext/ado"
 
 
 
 
-
-
+#' Check whether a path is under SVN control
+#'
+#' @param path_to_repo
+#'
+#' @return
+#'
+.svn_control <- function(path_to_repo = NULL){
+  if(is.null(path_to_repo)) path_to_repo <- ""
+  return <- tryCatch(system(paste("svn info", path_to_repo), intern = TRUE))
+  if(!is.null(attributes(return)$status)){
+    x <- FALSE
+  } else {
+    x <- TRUE
+  }
+  x
+}
+# path_to_repo <- "C:\\Users\\haynes\\Documents\\ClinicalStudies/690_PreferenceSensitiveCare_NRP74/21_Statistics_690/stataR_hyst"
+# .svn_control(path_to_repo)
